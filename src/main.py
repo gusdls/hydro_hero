fe3e00fe5146c37c93172660ee816558f526365b
Dohyun Kim <0704dohyun@gmail.com>
2023-09-09 08:08:00 +0900

    feat(player): make player can move and animate

--- src/main.py
+++ src/main.py
@@ -2,18 +2,27 @@ import pygame
 import sys
 
 from settings import *
+from sprites.player import Player
 
 class Game:
     def __init__(self):
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption("The Flumen")
         self.clock = pygame.time.Clock()
+        self.visible_group = pygame.sprite.Group()
+
+    def init_sprites(self):
+        self.player = Player(size=(150, 150), position=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+        self.visible_group.add(self.player)
 
     def draw_window(self):
         self.screen.fill(BLACK)
+        self.visible_group.update()
+        self.visible_group.draw(self.screen)
         pygame.display.update()
 
     def run(self):
+        self.init_sprites()
         while True:
             self.clock.tick(FPS)
             for event in pygame.event.get():
---
+++ src/sprites/player.py
@@ -0,0 +1,69 @@
+import pygame
+import glob
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, size, position):
+        super().__init__()
+        ANIMATIONS = ['idle', 'run']
+        ANIMATION_TIMES = [2000, 1000]
+        
+        self.animations = {}
+        self.animation_times = {}
+        for i, status in enumerate(ANIMATIONS):
+            self.import_assets(status, size)
+            self.animation_times[status] = ANIMATION_TIMES[i]
+
+        self.frame_index = 0
+        self.status = 'idle'
+        self.image = self.animations[self.status][self.frame_index]
+        self.rect = self.image.get_rect(center=position)
+
+        self.update_time = pygame.time.get_ticks()
+        self.facing_right = True
+
+        self.direction = pygame.math.Vector2()
+        self.speed = 10
+        
+    def import_assets(self, status, size):
+        files = glob.glob(f"assets/bandit/{status}*.png")
+        images = [pygame.image.load(file).convert_alpha() for file in files]
+        self.animations[status] = [pygame.transform.scale(image, size) for image in images]
+        
+    def animate(self):
+        animation_cooldown = self.animation_times[self.status] / len(self.animations[self.status])
+        if pygame.time.get_ticks() - self.update_time >= animation_cooldown:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+
+        if self.frame_index >= len(self.animations[self.status]):
+            self.frame_index = 0
+
+        self.image = self.animations[self.status][self.frame_index]
+        self.image = pygame.transform.flip(self.image, self.facing_right, False)
+        
+    def get_status(self):
+        if self.direction.x != 0:
+            self.status = 'run'
+        else:
+            self.status = 'idle'
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.direction.x = -1
+            self.facing_right = False
+        elif keys[pygame.K_d]:
+            self.direction.x = 1
+            self.facing_right = True
+        else:
+            self.direction.x = 0
+
+    def move(self):
+        self.rect.x += self.direction.x * self.speed
+
+    def update(self):
+        self.input()
+        self.get_status()
+        self.animate()
+        self.move()
